db732d35d937782a55a42c62b045fb089fc44293
KozielPiotr <koziel.piotr1988@gmail.com>
2019-12-03 10:07:53 +0100

    Database model created

--- rental_app/models.py
+++ rental_app/models.py
@@ -1,3 +1,21 @@
+"""Database model for rental_app"""
+
 from django.db import models
 
-# Create your models here.
+
+class Friend(models.Model):
+    """A friend who borrows something"""
+    name = models.CharField(max_length=100)
+
+
+class Belonging(models.Model):
+    """Thing that is being borrowed"""
+    name = models.CharField(max_length=100)
+
+
+class Borrowed(models.Model):
+    """What is being borrowed, to who, when and when returned"""
+    what = models.ForeignKey(Belonging, on_delete=models.CASCADE)
+    to_who = models.ForeignKey(Friend, on_delete=models.CASCADE)
+    when = models.DateTimeField(auto_now=True)
+    returned = models.DateTimeField(null=True, blank=True)
